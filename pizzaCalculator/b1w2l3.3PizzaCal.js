03f78d70ecfae9eeee83ad86bec1d2ab2389f046
chaotic-mo <70692259+chaotic-mo@users.noreply.github.com>
2020-10-16 11:44:33 +0200

    Add files via upload

---
+++ pizzaCalculator/b1w2l3.3PizzaCal.js
@@ -0,0 +1,25 @@
+// begin hier je JavaScript commandos
+// Motes Raji
+// Software Develpment
+	alert ("Je krijgt een paar vragen over je pizza keuze.")
+var small =	prompt ("hoeveel small pizza's wil je?")
+var midium = prompt ("hoeveel midium pizza's wil je?")
+var larg =	prompt ("hoeveel larg pizza's wil je?")
+	const SMALLPRICE = 5;
+	const MIDIUMPRICE = 11;
+	const LARGPRICE = 23;
+alert ("de totaal prijs van de small pizza's is:" + SMALLPRICE * small)
+alert ("de totaal prijs van de small pizza's is:" + MIDIUMPRICE * midium)
+alert ("de totaal prijs van de small pizza's is:" + LARGPRICE * larg)
+alert ("de totataal prijs is:" +(SMALLPRICE * small + MIDIUMPRICE * midium + LARGPRICE * larg) )
+var tafel1 = vraag * 1;
+var tafel2 = vraag * 2;
+var tafel3 = vraag * 3;
+var tafel4 = vraag * 4;
+var tafel5 = vraag * 5;
+var tafel6 = vraag * 6;
+var tafel7 = vraag * 7;	
+var tafel8 = vraag * 8;
+var tafel9 = vraag * 9;
+var tafel10 = vraag * 10;
+document.getElementById("demo").innerHTML = tafel1 + "<br>"+ tafel2 + "<br>" + tafel3 + "<br>"+ tafel4 + "<br>" + tafel5 + "<br>" + tafel6 + "<br>" + tafel7 + "<br>" + tafel8 + "<br>" + tafel9 + "<br>" + tafel10  ;
